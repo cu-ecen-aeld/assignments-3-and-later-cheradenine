e8d49c4122cd2913714ef74b5192461856e39317
cheradenine <john.burkhardt@gmail.com>
2023-01-08 08:32:57 -0500

    Specifies correct number of args for argc to look for

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -25,7 +25,7 @@ int write_content(const char* file_path, const char* content) {
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
+    if (argc < 3) {
         printf("Usage: writer <file> <content>\n");
         return 1;
     }
